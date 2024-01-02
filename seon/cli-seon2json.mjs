442ef44254ed339107c05b68ad40537978b3009e
atsuo yamada <yamada-git2@tir.jp>
2024-01-03 06:40:22 +0900

    feat: Add `--show-error-stacktrace` option

--- seon/cli-seon2json.mjs
+++ seon/cli-seon2json.mjs
@@ -4,6 +4,7 @@
 import fs from 'node:fs';
 import process from "node:process";
 import path from 'node:path';
+import { parseArgs } from "node:util";
 
 
 import * as seonUtil from 'seon/util';
@@ -12,9 +13,16 @@ import * as seonUtil from 'seon/util';
 const mkdirp = (path) => fs.mkdirSync(path, {recursive: true, mode: 0o755});
 
 
-const convertSeonToJson = (srcPath, dstPath) => {
+const convertSeonToJson = (srcPath, dstPath, isShowErrorStacktrace) => {
   const seonString = fs.readFileSync(srcPath, "utf-8");
-  const jsonString = seonUtil.convertSeonStringToJsonString(seonString, dstPath, srcPath);
+  let jsonString;
+  try {
+    jsonString = seonUtil.convertSeonStringToJsonString(seonString, srcPath);
+  } catch (e) {
+    if (isShowErrorStacktrace) { console.log(e) }
+    console.log("Error: " + e.message);
+    process.exit(1);
+  }
   mkdirp(path.dirname(dstPath));
   fs.writeFileSync(dstPath, jsonString);
 };
@@ -30,23 +38,46 @@ const exchangeExt = (targetPath, newExt) => {
 
 const displayUsageAndExit = () => {
   console.log(`usage:
-    npx seon2json [path/to/src.seon] [path/to/dst.json]`);
+    npx seon2json path/to/src.seon [path/to/result.json]
+      [-s --show-error-stacktrace] : display stacktrace on error (for debug)
+      [-h --help] : show help`);
   process.exit(1);
 };
 
 
-const main = (srcPath, dstPath) => {
+const parseConfig = {
+  allowPositionals: true,
+  options: {
+    "help": {
+      type: "boolean",
+      short: "h",
+    },
+    "show-error-stacktrace": {
+      type: "boolean",
+      short: "s",
+    },
+  },
+};
+
+
+const main = () => {
+  const cmdArgs = parseArgs(parseConfig);
+  const { help } = cmdArgs.values;
+  const isShowErrorStacktrace = cmdArgs.values["show-error-stacktrace"];
+  let [srcPath, dstPath] = cmdArgs.positionals;
+  if (help) { displayUsageAndExit() }
   if (!srcPath) { displayUsageAndExit() }
   if (!dstPath) {
     dstPath = exchangeExt(srcPath, ".json");
     if (srcPath === dstPath) { displayUsageAndExit() }
   }
   if (!fs.existsSync(srcPath)) {
-    console.log(`srcPath not found: ${srcPath}`);
+    console.log(`file not found: ${srcPath}`);
     process.exit(1);
   }
-  convertSeonToJson(srcPath, dstPath);
+  convertSeonToJson(srcPath, dstPath, isShowErrorStacktrace);
+  console.log(`Done. Wrote to ${dstPath}`);
 };
 
 
-main(process.argv[2], process.argv[3]);
+main();
