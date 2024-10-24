8dd200a6e3cde4b0ac4f86cf33f8f8a4878b5325
atsuo yamada <yamada-git2@tir.jp>
2024-10-25 08:54:14 +0900

    fix: Prevent to terminate watcher process by unhandled rejections

--- seon2js/test/watch.mjs
+++ seon2js/test/watch.mjs
@@ -3,10 +3,6 @@
 import * as Sh from "seon2js/sh";
 
 
-//import * as Fs from 'node:fs';
-//import * as Path from "node:path";
-//import * as Os from "node:os";
-//import * as NodeUtil from "node:util";
 import * as Process from "node:process";
 import * as ChildProcess from "node:child_process";
 
@@ -19,29 +15,8 @@ const shAsync = async (shCode) => {
 };
 
 
-//const baseTestAsync = async () => {
-//  await shAsync(`npx rimraf tmp`);
-//  await shAsync(`npx seon2js-build --src-dir scripts --dst-dir tmp`);
-//  await shAsync(`node tmp/generate-seon2js-vim.mjs > tmp/seon2js.vim`);
-//  // TODO: 外部コマンドのdiffに頼らないチェックに直す事
-//  await shAsync(`diff tmp/seon2js.vim ../ftdetect/seon2js.vim`);
-//  await shAsync(`npx rimraf tmp`);
-//  await shAsync(`npx seon2js-build --src scripts/generate-seon2js-vim.s2mjs --dst-dir tmp`);
-//  await shAsync(`node tmp/generate-seon2js-vim.mjs > tmp/seon2js.vim`);
-//  // TODO: 外部コマンドのdiffに頼らないチェックに直す事
-//  await shAsync(`diff tmp/seon2js.vim ../ftdetect/seon2js.vim`);
-//  await shAsync(`npx rimraf tmp`);
-//};
-
-
-//const bundleTestAsync = async () => {
-//  await shAsync(`npx rimraf tmp`);
-//  await shAsync(`npx seon2js-build --src-dir=test/bundle --dst-dir=tmp/src --bundle-out-file=tmp/test-bundle.js --bundle-entry-point=test/bundle/foo.mjs`);
-//  await shAsync(`node tmp/test-bundle.js | grep ababa > /dev/null`);
-//  await shAsync(`npx rimraf tmp`);
-//};
-
 const sleepAsync = (msec) => (new Promise((r) => setTimeout(r, msec)));
+
 
 const cmds = [
   'npx',
@@ -51,35 +26,46 @@ const cmds = [
     '--dst-dir=tmp/src',
     '--watch',
   ],
-  //{detached: true}, // NB: この引数を与えるべきなのかよくわからなかった
+  //{detached: true}, // これ入れても意味ない？
 ];
+
 
 const main = async () => {
   await shAsync(`npx rimraf tmp`);
-  const process = ChildProcess.spawn(... cmds);
-  process.stdout.on('data', (data) => console.log(data.toString()));
-  process.stderr.on('data', (data) => console.error(data.toString()));
-  process.on('close', (code, signal) => console.log(`closed by code=${code} signal=${signal}`));
-  process.on('exit', (code) => console.log(`exited by code=${code}`));
-  process.on('error', (err) => console.error(err.toString()));
+  const child = ChildProcess.spawn(... cmds);
+  let runningLog = '';
+  const processData = (data) => {
+    const msg = data.toString();
+    if (runningLog) { runningLog += "\n" }
+    runningLog += msg;
+    console.log(msg);
+  };
+  child.stdout.on('data', processData);
+  child.stderr.on('data', processData);
+  child.on('close', (code, signal) => console.log(`closed by code=${code} signal=${signal}`));
+  child.on('exit', (code) => console.log(`exited by code=${code}`));
+  child.on('error', (err) => console.error(err.toString()));
   await sleepAsync(1000);
-  const isNotWatched = process.killed || (process.exitCode !== null);
-  if (!isNotWatched) {
-    console.log('process is alive, do to stop ...');
-    process.kill('SIGHUP');
-    await sleepAsync(100);
+  if (!runningLog) {
+    // TODO: よくわからないが、1000msec待っても子プロセス側のトランスパイル処理が実行されない時がある(子プロセスは起動しているがstdoutに処理内容のログが出ない)。本当はちゃんと直さないといけないのだけど、このtestは手でしか実行しないので、このエラーが出たら再度実行し直せばすむ。今は放置する。あとでCI実行とかするようになったら原因を調べて直す事！
+    throw new Error('could not bootstrap child transpiler, please retry');
   }
-  await shAsync(`npx rimraf tmp`);
-  await sleepAsync(100);
-  if (isNotWatched) {
-    console.error('terminated watcher process of seon2js-build !!!');
+  if (child.killed || (child.exitCode !== null)) {
+    console.error('FAILED: child process were already shutdowned !!!');
+    await shAsync(`npx rimraf tmp`);
     await sleepAsync(100);
     Process.exit(1);
-  } else {
-    console.log('all done.');
-    await sleepAsync(100);
-    Process.exit(0);
   }
+  console.log('process is alive, do to stop ...');
+  child.kill('SIGHUP');
+  await sleepAsync(1000);
+  //child.disconnect();
+  //child.unref();
+  console.log('all ok.');
+  await shAsync(`npx rimraf tmp`);
+  await sleepAsync(100);
+  // TODO: 明示的にexitしないとプロセスが終わらない。子process関連で何か終了を阻害するものがあるっぽいのだけどよく分からない。テスト用途ではexit(0)でも問題ないので、将来に時間のある時の対応としたい
+  Process.exit(0);
 };
 
 
